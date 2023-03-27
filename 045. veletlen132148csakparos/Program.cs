3b474a70dedc613ac23cf29fb3fd9c882ac442b9
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:49:35 +0200

    132-től 148-ig csak páros véletlen számok

---
+++ 045. veletlen132148csakparos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _045.veletlen132148csakparos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            for (int i = 132; i < 149; i++)    // 9 darab páros számot fog generálni véletlen sorrendben, mert a 132 és a 148 között 9 db páros szám van. De van amelyiket kétszer is legenerálja.
+
+                if (i % 2 == 0)
+
+                {
+                    Console.WriteLine(random.Next(132, 149));
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
